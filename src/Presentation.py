706e67e0309b0725888566a1d7334b72ecbbecfb
YWTCN <xcn_tyw@sjtu.edu.cn>
2024-02-07 17:18:38 +0100

    &

--- src/Presentation.py
+++ src/Presentation.py
@@ -13,27 +13,34 @@ os.chdir("D:/Program/Github/User-Scheduling-in-5G/src")
 ###Question 3、4、5
 def qfive(path):
     N,K,M,p,P,R = readData(path)
-    print(P)
     if prePro(N,K,M,p,P,R):
         D = []
+        D1 = []
+        D2 = []
         for i in range(N):
             D.append(Channel(i,K,M,P,R))
-        print("IP-dominated Removing results:")
+            D1.append([])
+            D2.append([])
+        print("Instance after IP-dominated Removing:")
         for i in range(N):
             print()
             print(f"{i+1}th channel:")
-            rem,D[i] = D[i].IpRe()
-            for j in range(len(D[i])):
-                print(D[i][j].index)
-        print("Then, LP-dominated Removing results(already removed IP-dominated):")
+            rem,A = D[i].IpRe()
+            D1[i].append(rem)
+            for j in range(len(A)):
+                print(A[j].index,end = ' ')
+        print("\n\nThen, instance after LP-dominated Removing(already removed IP-dominated):")
         for i in range(N):
             print()
             print(f"{i+1}th channel:")
-            rem,D[i] = D[i].LpRe()
-            for j in range(len(D[i])):
-                print(D[i][j].index)
+            rem,A = D[i].LpRe()
+            D2[i].append(rem)
+            for j in range(len(A)):
+                print(A[j].index,end = ' ')
 for i in range(1,6):
+    print(f"Test{i}'s results:")
     qfive(f"test{i}.txt")
+    print("\n\n")
 
 
 # def qseven(path):
--- src/scheduling.py
+++ src/scheduling.py
@@ -10,10 +10,6 @@ class User():
     
     def __init__(self, i1,i2, p, r):
         self.index = (i1,i2)
-        print(p)
-        print(r)
-        for i,value in enumerate(zip(p,r)):
-            print(i,value)
         self.powers = [Power(i1,i2,i+1, value[0],value[1]) for i, value in enumerate(zip(p, r))]
 
 
@@ -26,8 +22,7 @@ class Channel():
         self.index = ind+1
         self.M = M
         self.K = K
-        print(P)
-        # self.users = [User(ind + 1, i + 1, P[ind][i], R[ind][i]) for i in range(len(P[ind]))]
+        self.length = K*M
         self.users = [User(ind+1,i+1, value[0], value[1]) for i, value in enumerate(zip(P[ind], R[ind]))]
 
     # def remove(self, ind):
@@ -56,19 +51,28 @@ class Channel():
                 ins = res.pop()
                 rem.append(ins)
                 self.users[ins.index[1]-1].powers.remove(ins)
+                self.length-=1
         return rem,res
     
     def LpRe(self):
         rem,arr = self.IpRe()
         S = [arr[0],arr[1]]
-        d = (arr[1].r-arr[0].r)/(arr[1].p-arr[0].p)
+        if arr[1].p-arr[0].p != 0:
+            d = (arr[1].r-arr[0].r)/(arr[1].p-arr[0].p)
+        else:
+            d = 1000000.00
         for i in range(2,len(arr)):
             for j in S[::-1]:
-                if (arr[i].r-j.r)/(arr[i].p-j.p)>=d:
-                    d= (arr[i].r-j.r)/(arr[i].p-j.p)
+                if arr[i].p-j.p == 0:
+                    ratio = 1000000.00
+                else:
+                    ratio = (arr[i].r-j.r)/(arr[i].p-j.p)
+                if ratio>=d:
+                    d = ratio
                     ins = S.pop()
                     rem.append(ins)
                     self.users[ins.index[1]-1].powers.remove(ins)
+                    self.length-=1
             S.append(arr[i])
         return rem,S        
     
